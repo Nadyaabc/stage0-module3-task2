ddcf4052bf5ecd0866347b0d18420d22fa80bf90
Nadyaabc <67741014+Nadyaabc@users.noreply.github.com>
2024-04-09 20:14:42 +0300

    Update NamingConvention.java

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -3,13 +3,13 @@ package school.mjc.stage0.module3.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variable = 10;
-        int variable_One = 1;
-        int variable_Two = 2;
-        int variable_three = 3;
+        int variableOne = 1;
+        int variableTwo = 2;
+        int variableThree = 3;
         final int VARIABLE_FOUR = 4;
-        final int VARIABLE_fIVE = 5;
-        int Variable_Six = 6;
-        int variable_seven = 7;
-        int Variable_Eight = 8;
+        final int VARIABLE_FIVE = 5;
+        int VariableSix = 6;
+        int variableSeven = 7;
+        int VariableEight = 8;
     }
 }
